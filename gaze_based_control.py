9143340c0d460c6440996fb2152b2affc8523312
John Lyle <johnlyleiv@gmail.com>
2024-11-05 16:13:55 -0600

    base format just wanted to make a file

---
+++ gaze_based_control.py
@@ -0,0 +1,17 @@
+import numpy
+
+def gaze_location(image):
+    location = image
+    return location
+
+def agent_from_gaze(gaze_location):
+    desired_agent = gaze_location
+    return desired_agent
+
+def control_agent(agent):
+    control = agent
+    return control
+
+if __name__ == "__main__":
+    image = None
+    do_stuff = control_agent(agent_from_gaze(gaze_location(image)))
