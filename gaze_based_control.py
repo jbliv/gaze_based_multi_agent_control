4098cd27d4dc4409a95a8b173d1dc971fc1b9033
John Lyle <johnlyleiv@gmail.com>
2024-12-02 19:48:21 -0600

    fixed file name pt 2

--- gaze_based_control.py
+++ gaze_based_control.py
@@ -1,23 +1,27 @@
 import numpy
-import agent_controller_2
+import agent_controller
 import gaze2agent
+
 
 def gaze_location(image):
     location = image
     return location
 
+
 def agent_from_gaze(gaze_location):
     desired_agent = gaze_location
     return desired_agent
+
 
 def control_agent(agent):
     control = agent
     return control
 
+
 if __name__ == "__main__":
     image = None
     # Initialization code, whether that be a calibration sequence or accessing a stored calibration sequence
-    controller = agent_controller_2.DualWindowController()
+    controller = agent_controller.DualWindowController()
     agent1 = controller.agents[0]
     agent2 = controller.agents[1]
     agent_selector = gaze2agent.agent_select(agent1, agent2, "position", 60)
@@ -25,4 +29,4 @@ if __name__ == "__main__":
     # Loop for standard operations, exit condition tbd
     running = True
     while running:
-        agent_selector.getAgent() # input is gaze location
+        agent_selector.getAgent()  # input is gaze location
